9e707b39168a004d3df90dd2da0f01a92f6d368e
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-01-17 01:02:36 +0330

    commit array Methods

---
+++ NewFolder4/arryaMethods.js
@@ -0,0 +1,31 @@
+let students = [
+    "bande",
+    "Javad",
+    "Pedram",
+    "Mina",
+    "Mazloom Hossein",
+    "Mazloom Mehdi",
+  ];
+
+
+
+  ////push()
+//   students.push("reza" ,23 , "1377")
+//   console.log(students);
+
+
+
+/////////POP()
+// students.pop()
+// console.log(students);
+
+
+////////shift()
+// students.shift()
+// console.log(students);
+
+
+
+///////unshift()
+// students.unshift("reza" ,23 , "1377")
+//   console.log(students);
