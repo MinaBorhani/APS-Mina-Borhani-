e7caae5b6ebb1a7ce77efd03c9517460377aa59a
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-01-17 22:41:41 +0330

    commit Quiz Traffic

---
+++ NewFolder4/QuizTraffic.js
@@ -0,0 +1,10 @@
+let traffic=prompt("What is traffic signal?")
+if(traffic==="stop"){
+    alert("Red");
+}else if(traffic === "slow"){
+    alert("yellow");
+}else if(traffic==="go"){
+    alert("green");
+}else{
+    alert("purple");
+}
