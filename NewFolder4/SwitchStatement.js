4ade117ec3b28c5aa45878b54cc6fae864140adb
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-01-17 22:49:15 +0330

    commit Switch Statement

---
+++ NewFolder4/SwitchStatement.js
@@ -0,0 +1,21 @@
+let day=20;
+switch(day){
+        case 0:
+        console.log("sunday");
+        break;
+        case 1:
+        console.log("monday");
+        break;
+        case 2:
+        console.log("tusday");
+        break;
+        case 3:
+        console.log("wensday");
+        break;
+        case 4:
+        console.log("trisday");
+        break;
+        default:
+            console.log("stop");
+
+}
