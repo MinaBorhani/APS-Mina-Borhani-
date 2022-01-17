2b6aa36f454524695fdd5e8ddaabd2b5fded5c6b
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-01-17 23:26:42 +0330

    commit conditional

---
+++ NewFolder5/conditional.js
@@ -0,0 +1,40 @@
+//if
+
+
+let mony=2;
+
+// if(mony) console.log("rich");
+// console.log("poor");
+
+
+console.log(Boolean([]));   //true
+console.log(Boolean(undefined));   //fals
+console.log(Boolean(null));   //fals 
+console.log(Boolean(""));     //fals
+console.log(Boolean(0));  //true
+console.log(Boolean({}));    ///fals
+console.log(Boolean(NaN));     //true
+console.log(Boolean(2));   //fals
+
+
+
+
+
+
+
+
+
+
+//switch
+
+// switch(argument){
+//     case value1;
+//     case value2;
+    
+//     break;
+//     case value3;
+    
+//     break;
+//     default
+
+// }
